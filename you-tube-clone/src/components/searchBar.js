23d701cec1ec3639005701f70fd88f2e11dec330
Ali <ar.k@outlook.com>
2017-11-16 18:09:52 -0800

    create serachBar component

---
+++ you-tube-clone/src/components/searchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'React';
+
+class searchBar extends Component {
+  render() {
+    return (
+      <div>
+        <input />
+      </div>
+    )
+  }
+}
+
+export default searchBar;
